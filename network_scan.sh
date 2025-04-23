d0f8114ee5d5a8a5eb53a8423e7bbb3c55a34f67
ayoub <ayoub@kali.lucas.org>
2025-04-22 21:01:16 -0400

    Add basic network scanner script

---
+++ network_scan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Welcome to Network Scanner"
+echo "scanning your network: 192.168.94.0/24"
+echo "started at: $(date)"
+echo "<====== SCANNING ======>"
+
+# Start scan and save to file
+nmap -sn 192.168.94.0/24 | tee results.txt
+sleep 2
+
+echo "--------SCAN FINISHED--------"
+count=$(grep "Nmap scan report" results.txt | wc -l)
+echo ""
+echo "______ DEVICE TYPES DETECTED ______"
+echo ""
+echo "$(grep -A 5 "Nmap scan report" results.txt | grep -E "Nmap scan report|Device type|Running" | sed 's/^/🔍 /')"
+echo ""
+echo "=================================="
+echo "✅  Scan finished at: $(date)"
+echo "✅  Hosts found: $count"
+echo "=================================="
